49fd0df5c1e399571868a8fa63826bd9e852448e
Joanna <Bartek@MacBook-Pro-Bartosz.local>
2022-05-11 18:38:41 +0200

    readme.md prepared

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ const errorIcon = document.querySelector('.subscribe__form--error-icon')
 const subscribeForm = document.querySelector('.subscribe__form')
 
 const makeIfCorrect = () => {
-    errorIcon.classList.remove('activate')
-    msg.classList.add('msg-styles')
-    msg.textContent = 'Your email was added successfully!'
+	errorIcon.classList.remove('activate')
+	msg.classList.add('msg-styles')
+	msg.textContent = 'Your email was added successfully!'
 }
 
 makeIfNotCorrect = () => {
-    subscribeForm.classList.add('border-pink')
-    errorIcon.classList.add('activate')
-    msg.classList.add('msg-styles')
-    msg.textContent = 'Please provide a valid email'
+	subscribeForm.classList.add('border-pink')
+	errorIcon.classList.add('activate')
+	msg.classList.add('msg-styles')
+	msg.textContent = 'Please provide a valid email'
 }
 
 const checkIfCorrect = input => {
@@ -22,15 +22,13 @@ const checkIfCorrect = input => {
 		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
 	if (re.test(input.value)) {
-        makeIfCorrect()
-     } else {
-        makeIfNotCorrect()
+		makeIfCorrect()
+	} else {
+		makeIfNotCorrect()
 	}
-	
 }
 
 btn.addEventListener('click', e => {
 	e.preventDefault()
 	checkIfCorrect(email)
 })
-
